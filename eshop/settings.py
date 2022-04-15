6345726b49f4721e0dcfe959d6349b3fd12f0719
muthui <muthuikyalozis1@gmail.com>
2022-04-15 12:09:11 +0300

    third commit complete

--- eshop/settings.py
+++ eshop/settings.py
@@ -133,6 +133,18 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# smtp configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'smtpkyalosite@gmail.com'
+EMAIL_HOST_PASSWORD = '1234567fkq.'
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
